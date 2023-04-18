44fb2e2652b9d11c32d2ac68074d1d4eeee36444
Ruben Serrano Izquierdo <rserranoizq@gmail.com>
2023-04-18 19:16:19 +0200

    Add settings for local dev with automatic webhook update

---
+++ dev.js
@@ -0,0 +1,22 @@
+const dotenv = require('dotenv');
+const wrangler = require('wrangler');
+const {tunnel} = require('cloudflared');
+const {Telegraf} = require("telegraf");
+
+const PORT = 8080;
+
+dotenv.config({path: './.dev.vars'});
+
+(async () => {
+    const {url, stop} = tunnel({'--url': `localhost:${PORT}`})
+    const service = await wrangler.unstable_dev('./dist/worker.js', {
+        config: './wrangler.toml',
+        port: PORT,
+        logLevel: "info",
+        vars: process.env,
+        inspect: true,
+    });
+    const bot = new Telegraf(process.env.BOT_TOKEN);
+    await bot.telegram.setWebhook(await url);
+    await bot.telegram.getWebhookInfo().then(console.log);
+})();
